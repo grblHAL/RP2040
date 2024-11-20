4b3b637440c67b443663f4afe0a25705bebedc14
Terje Io <terjeio@users.noreply.github.com>
2024-11-20 19:17:46 +0100

    Added support for "assorted small plugins".

--- my_machine.h
+++ my_machine.h
@@ -51,7 +51,7 @@
 //#define SPINDLE2_ENABLE         SPINDLE_NONE
 //#define SPINDLE_OFFSET          1 // Set to 1 to add offset move when switching between laser and spindle
 // **********************
-//#define MODBUS_ENABLE           1 // Set to 1 for auto direction, 2 for direction signal on auxillary output pin.
+//#define MODBUS_ENABLE           1 // Set to 1 for auto direction, 2 for direction signal on auxiliary output pin.
 //#define WIFI_ENABLE             0 // Do NOT enable here, enable in CMakeLists.txt!
 //#define WIFI_SOFTAP             1 // Use Soft AP mode for WiFi. NOTE: WIP - not yet complete!
 //#define ETHERNET_ENABLE         0 // Do NOT enable here, enable in CMakeLists.txt!
@@ -80,9 +80,12 @@
 //#define STEP_INJECT_ENABLE      1
 //#define MCP3221_ENABLE       0x4D // Enable MCP3221 I2C ADC input with address 0x4D (0b01001101).
 //#define RGB_LED_ENABLE          2 // Set to 1 to enable strip length settings $536 and $537, set to 2 to also enable M150 LED strip control.
-//#define PWM_SERVO_ENABLE        1 // Enable PWM servo support, requires at least one PWM capable auxillary output.
-//#define BLTOUCH_ENABLE          1 // Enable BLTouch support. Requires and claims one PWM servo output.
-//#define EVENTOUT_ENABLE         1 // Enable binding events (triggers) to control auxillary outputs.
+//#define PWM_SERVO_ENABLE        1 // Enable M280 PWM servo support, requires at least one PWM capable auxiliary output.
+//#define BLTOUCH_ENABLE          1 // Enable M401/M402 BLTouch support. Requires and claims one auxiliary PWM servo output.
+//#define EVENTOUT_ENABLE         1 // Enable binding events (triggers) to control auxiliary outputs.
+//#define ESP_AT_ENABLE           1 // Enable support for Telnet communication via UART connected ESP32 running ESP-AT.
+//#define FEED_OVERRIDE_ENABLE    1 // Enable M200 feed override control.
+//#define HOMING_PULLOFF_ENABLE   1 // Enable per axis homing pulloff distance settings.
 
 
 // Optional control signals:
