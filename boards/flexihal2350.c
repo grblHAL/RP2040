f9bb62cc44415f6760a5c6c4093bdb2fe76a9343
Mitchell Grams <mitch.grams@gmail.com>
2026-05-19 21:12:09 -0600

    Move probe select to board specific code

--- boards/flexihal2350.c
+++ boards/flexihal2350.c
@@ -25,8 +25,10 @@
 
 #if defined(BOARD_FLEXIHAL2350)
 
+#include "grbl/hal.h"
 #include "grbl/task.h"
 #include "grbl/state_machine.h"
+#include "grbl/pin_bits_masks.h"
 
 //static driver_setup_ptr driver_setup;
 
@@ -42,6 +44,15 @@ typedef struct {
 } motor_pins_t;
 
 motor_pins_t fault_signals = {};
+
+static probe_select_ptr probe_select;
+static probe_configure_ptr probe_configure;
+
+probe_id_t active_probe = Probe_Default;
+
+// TODO: add code guards and assign pins based on board map instead of hardcoding?
+aux_ctrl_t probe_pin = { .function = Input_Probe, .port = IOPORT_UNASSIGNED, .gpio.pin = 4 };
+aux_ctrl_t toolsetter_pin = { .function = Input_Toolsetter, .port = IOPORT_UNASSIGNED, .gpio.pin = 3 };
 
 void input_add_expander_pin (xbar_t *input)
 {
@@ -111,6 +122,79 @@ static void driverSetup (void *data)
     }
 }
 
+bool onProbeSelect (probe_id_t probe_id)
+{
+    gpio_in_config_t config0 = {0};
+    gpio_in_config_t config1 = {0};
+
+    switch(probe_id) {
+
+        case Probe_Default:
+            ioport_enable_irq(probe_pin.port, (pin_irq_mode_t)IRQ_Mode_All, NULL);
+            ioport_enable_irq(toolsetter_pin.port, (pin_irq_mode_t)IRQ_Mode_None, NULL);
+            break;    
+
+        case Probe_Toolsetter:
+            ioport_enable_irq(probe_pin.port, (pin_irq_mode_t)IRQ_Mode_None, NULL);
+            ioport_enable_irq(toolsetter_pin.port, (pin_irq_mode_t)IRQ_Mode_All, NULL);
+            break;
+
+        default:
+            return false;
+    }
+
+    active_probe = probe_id;
+    hal.probe.configure(false, false);
+
+    return true;
+}
+
+static void probeConfigure (bool is_probe_away, bool probing)
+{
+    if (active_probe == Probe_Toolsetter) {
+        if (settings.probe.invert_toolsetter_input != settings.probe.invert_probe_pin)
+            is_probe_away = !is_probe_away; // invert here only if toolsetter invert setting is different from probe invert setting
+    }
+    probe_configure(is_probe_away, probing);
+}
+
+#if PROBE_ENABLE == 2 && NGC_PARAMETERS_ENABLE
+
+void probe_select_init (void)
+{
+    bool ok = true;
+    xbar_t *pin;
+
+    if(ioports_enumerate(Port_Digital, Port_Input, (pin_cap_t){.external = On, .claimable = On }, __find_in_ext, &probe_pin)){
+        if(pin = ioport_claim(Port_Digital, Port_Input, &probe_pin.port, NULL)) {
+            ioport_set_description(Port_Digital, Port_Input, probe_pin.port, "expander multiplex");
+            ioport_set_function(pin, probe_pin.function, NULL);
+        }
+    } else 
+        ok = false;
+
+    if(ioports_enumerate(Port_Digital, Port_Input, (pin_cap_t){.external = On, .claimable = On }, __find_in_ext, &toolsetter_pin)){
+        if(pin = ioport_claim(Port_Digital, Port_Input, &toolsetter_pin.port, NULL)) {
+            ioport_set_description(Port_Digital, Port_Input, toolsetter_pin.port, "expander multiplex");
+            ioport_set_function(pin, toolsetter_pin.function, NULL);
+        }
+    } else 
+        ok = false;
+    
+    if(ok && (hal.driver_cap.toolsetter = On)) {
+        probe_select = hal.probe.select;
+        hal.probe.select = onProbeSelect;
+
+        probe_configure = hal.probe.configure;
+        hal.probe.configure = probeConfigure;
+
+    } else
+        task_run_on_startup(report_warning, "Probe expander plugin: error claiming required ports.");
+
+}
+
+#endif // PROBE_ENABLE == 2 && NGC_PARAMETERS_ENABLE
+
 void board_init (void)
 {
     task_run_on_startup(driverSetup, NULL);
@@ -120,4 +204,4 @@ void board_init (void)
 
 }
 
-#endif
+#endif // BOARD_FLEXIHAL2350
